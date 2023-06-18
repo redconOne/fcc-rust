8e858dd945b651d01943cc966e0763bb244fcb57
redconOne <mingleeng@gmail.com>
2023-06-17 18:40:55 -0600

    initial: initial commit

---
+++ calculator/target/debug/deps/calculator-092f3c2fd3b98744.d
@@ -0,0 +1,5 @@
+C:\Users\mingl\Documents\Programming\fcc-rust\calculator\target\debug\deps\calculator-092f3c2fd3b98744.rmeta: src\main.rs
+
+C:\Users\mingl\Documents\Programming\fcc-rust\calculator\target\debug\deps\calculator-092f3c2fd3b98744.d: src\main.rs
+
+src\main.rs:
