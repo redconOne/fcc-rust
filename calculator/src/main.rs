6b483d8bdbad43b4a52a0267bddb7bb170bc8595
redconOne <mingleeng@gmail.com>
2023-06-19 19:24:05 -0600

    feat: added arg reading

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,6 +1,11 @@
 use std::env::{args, Args};
 
 fn main() {
-    let args: Args = args();
-    println!("{}", args);
+    let mut args: Args = args();
+
+    let first: String = args.nth(1).unwrap();
+    let operator: String = args.nth(0).unwrap();
+    let second: String = args.nth(0).unwrap();
+
+    println!("{}{}{}", first, operator, second);
 }
