8e89749071aaed14a90cd70c022c0e5a56ec6c00
redconOne <mingleeng@gmail.com>
2023-06-21 01:22:41 -0600

    feat: added output function to generate properly formatted String

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -4,14 +4,14 @@ fn main() {
     let mut args: Args = args();
 
     let first: String = args.nth(1).unwrap();
-    let operator: String = args.nth(0).unwrap();
+    let operator: char = args.nth(0).unwrap().chars().next().unwrap();
     let second: String = args.nth(0).unwrap();
 
-    let first_number = first.parse::<f32>().unwrap();
-    let second_number = second.parse::<f32>().unwrap();
-    let result: f32 = 3.0;
+    let first_number: f32 = first.parse::<f32>().unwrap();
+    let second_number: f32 = second.parse::<f32>().unwrap();
+    let result: f32 = operate(operator, first_number, second_number);
 
-    println!("{} {} {} = {}", first, operator, second, result)
+    println!("{}", output(first_number, operator, second_number, result));
 }
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
@@ -22,4 +22,11 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
         '*' | 'x' | 'X' => first_number * second_number,
         _ => panic!("Invalid operator used."),
     }
+}
+
+fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+    format!(
+        "{} {} {} = {}",
+        first_number, operator, second_number, result
+    )
 }
