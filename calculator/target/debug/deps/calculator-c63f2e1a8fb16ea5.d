9d53e4361155221f5619eeb2fd00c2b099a1a143
redconOne <mingleeng@gmail.com>
2023-06-24 02:51:54 -0600

    remove: deleted target folder

--- calculator/target/debug/deps/calculator-c63f2e1a8fb16ea5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mingl\Documents\Programming\fcc-rust\calculator\target\debug\deps\calculator-c63f2e1a8fb16ea5.rmeta: src\main.rs
-
-C:\Users\mingl\Documents\Programming\fcc-rust\calculator\target\debug\deps\calculator-c63f2e1a8fb16ea5.d: src\main.rs
-
-src\main.rs:
